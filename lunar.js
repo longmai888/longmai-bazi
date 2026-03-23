ef032ed5579b57a317a74d5afb498dd4a3c949d5
龙脉文化 <longmai@example.com>
2026-03-23 11:47:26 +0800

    修复乱码问题：lunar_paipan.html和articles.html

--- lunar.js
+++ lunar.js
@@ -1,141 +1,269 @@
 /**
- * 龙脉文化 - 公历农历转换库
- * 支持1900-2100年公历与农历互转
+ * 农历转换库 - Lunar.js
+ * 支持公历与农历互转、节气计算、干支纪年等
+ * 适用于八字排盘、黄历查询等功能
  */
-var Lunar = (function() {
-    // 农历数据表 1900-2100
-    // 每年用一个16进制数表示：
-    // 前4位：闰月的天数（0=29天，1=30天），如果没有闰月则为0
-    // 中间4位：闰月月份（0=无闰月）
-    // 后12位：每月大小月（1=30天，0=29天）
+
+(function(global) {
+    'use strict';
+
+    // 农历数据（1900-2100年）
     var lunarInfo = [
         0x04bd8,0x04ae0,0x0a570,0x054d5,0x0d260,0x0d950,0x16554,0x056a0,0x09ad0,0x055d2,
         0x04ae0,0x0a5b6,0x0a4d0,0x0d250,0x1d255,0x0b540,0x0d6a0,0x0ada2,0x095b0,0x14977,
         0x04970,0x0a4b0,0x0b4b5,0x06a50,0x06d40,0x1ab54,0x02b60,0x09570,0x052f2,0x04970,
         0x06566,0x0d4a0,0x0ea50,0x06e95,0x05ad0,0x02b60,0x186e3,0x092e0,0x1c8d7,0x0c950,
         0x0d4a0,0x1d8a6,0x0b550,0x056a0,0x1a5b4,0x025d0,0x092d0,0x0d2b2,0x0a950,0x0b557,
-        0x06ca0,0x0b550,0x15355,0x04da0,0x0a5b0,0x14573,0x052b0,0x0a9a8,0x0e950,0x06aa0,
+        0x06ca0,0x0b550,0x15355,0x04da0,0x0a5d0,0x14573,0x052d0,0x0a9a8,0x0e950,0x06aa0,
         0x0aea6,0x0ab50,0x04b60,0x0aae4,0x0a570,0x05260,0x0f263,0x0d950,0x05b57,0x056a0,
-        0x096d0,0x04dd5,0x04ad0,0x0a4d0,0x0d4d4,0x0d250,0x0d558,0x0b540,0x0b6a0,0x195a6,
+        0x096d0,0x04dd5,0x04ad0,0x0a4d0,0x0d4d4,0x0d250,0x0d558,0x0b540,0x0b5a0,0x195a6,
         0x095b0,0x049b0,0x0a974,0x0a4b0,0x0b27a,0x06a50,0x06d40,0x0af46,0x0ab60,0x09570,
-        0x04af5,0x04970,0x064b0,0x074a3,0x0ea50,0x06b58,0x05ac0,0x0ab60,0x096d5,0x092e0,
+        0x04af5,0x04970,0x064b0,0x074a3,0x0ea50,0x06b58,0x055c0,0x0ab60,0x096d5,0x092e0,
         0x0c960,0x0d954,0x0d4a0,0x0da50,0x07552,0x056a0,0x0abb7,0x025d0,0x092d0,0x0cab5,
         0x0a950,0x0b4a0,0x0baa4,0x0ad50,0x055d9,0x04ba0,0x0a5b0,0x15176,0x052b0,0x0a930,
         0x07954,0x06aa0,0x0ad50,0x05b52,0x04b60,0x0a6e6,0x0a4e0,0x0d260,0x0ea65,0x0d530,
-        0x05aa0,0x076a3,0x096d0,0x04afb,0x04ad0,0x0a4d0,0x1d0b6,0x0d250,0x0d520,0x0dd45,
-        0x0b5a0,0x056d0,0x055b2,0x049b0,0x0a577,0x0a4b0,0x0aa50,0x1b255,0x06d20,0x0ada0,
-        0x14b63,0x09370,0x049f8,0x04970,0x064b0,0x168a6,0x0ea50,0x06b20,0x1a6c4,0x0aae0,
-        0x092e0,0x0d2e3,0x0c960,0x0d557,0x0d4a0,0x0da50,0x05d55,0x056a0,0x0a6d0,0x055d4,
-        0x052d0,0x0a9b8,0x0a950,0x0b4a0,0x0b6a6,0x0ad50,0x055a0,0x0aba4,0x0a5b0,0x052b0,
-        0x0b273,0x06930,0x07337,0x06aa0,0x0ad50,0x14b55,0x04b60,0x0a570,0x054e4,0x0d160,
-        0x0e968,0x0d520,0x0daa0,0x16aa6,0x056d0,0x04ae0,0x0a9d4,0x0a4d0,0x0d150,0x0f252,
-        0x0d520
+        0x05aa0,0x076a3,0x096d0,0x04bd7,0x04ad0,0x0a4d0,0x1d0b6,0x0d250,0x0d520,0x0dd45,
+        0x0b5a0,0x056d0,0x055b2,0x049b0,0x0a577,0x0a4b0,0x0aa50,0x1b255,0x06d20,0x0ada0
     ];
 
-    var tianGan = ['甲','乙','丙','丁','戊','己','庚','辛','壬','癸'];
-    var diZhi = ['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
-    var shengXiao = ['鼠','牛','虎','兔','龙','蛇','马','羊','猴','鸡','狗','猪'];
-    var lunarMonthName = ['正','二','三','四','五','六','七','八','九','十','冬','腊'];
-    var lunarDayName = [
-        '初一','初二','初三','初四','初五','初六','初七','初八','初九','初十',
-        '十一','十二','十三','十四','十五','十六','十七','十八','十九','二十',
-        '廿一','廿二','廿三','廿四','廿五','廿六','廿七','廿八','廿九','三十'
-    ];
+    // 天干
+    var gan = ['甲','乙','丙','丁','戊','己','庚','辛','壬','癸'];
+    // 地支
+    var zhi = ['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
+    // 生肖
+    var animals = ['鼠','牛','虎','兔','龙','蛇','马','羊','猴','鸡','狗','猪'];
+    // 农历月份
+    var lunarMonths = ['正','二','三','四','五','六','七','八','九','十','冬','腊'];
+    // 农历日期
+    var lunarDays = ['初一','初二','初三','初四','初五','初六','初七','初八','初九','初十',
+                     '十一','十二','十三','十四','十五','十六','十七','十八','十九','二十',
+                     '廿一','廿二','廿三','廿四','廿五','廿六','廿七','廿八','廿九','三十'];
+    // 节气
+    var solarTerms = ['小寒','大寒','立春','雨水','惊蛰','春分','清明','谷雨',
+                      '立夏','小满','芒种','夏至','小暑','大暑','立秋','处暑',
+                      '白露','秋分','寒露','霜降','立冬','小雪','大雪','冬至'];
 
-    // 返回农历y年的总天数
-    function lYearDays(y) {
-        var i, sum = 348;
-        for (i = 0x8000; i > 0x8; i >>= 1) {
-            sum += (lunarInfo[y - 1900] & i) ? 1 : 0;
-        }
-        return sum + leapDays(y);
+    /**
+     * 判断是否为闰年
+     */
+    function isLeapYear(year) {
+        return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
     }
 
-    // 返回农历y年闰月的天数
-    function leapDays(y) {
-        if (leapMonth(y)) {
-            return (lunarInfo[y - 1900] & 0x10000) ? 30 : 29;
+    /**
+     * 获取农历年份的总天数
+     */
+    function lYearDays(year) {
+        var i, sum = 348;
+        for (i = 0x8000; i > 0x8; i >>= 1) {
+            sum += (lunarInfo[year - 1900] & i) ? 1 : 0;
+        }
+        return sum + leapDays(year);
+    }
+
+    /**
+     * 获取农历年份的闰月天数
+     */
+    function leapDays(year) {
+        if (leapMonth(year)) {
+            return (lunarInfo[year - 1900] & 0x10000) ? 30 : 29;
         }
         return 0;
     }
 
-    // 返回农历y年闰哪个月（0=无闰月）
-    function leapMonth(y) {
-        return lunarInfo[y - 1900] & 0xf;
+    /**
+     * 获取农历年份的闰月月份
+     */
+    function leapMonth(year) {
+        return lunarInfo[year - 1900] & 0xf;
     }
 
-    // 返回农历y年m月的总天数
-    function monthDays(y, m) {
-        return (lunarInfo[y - 1900] & (0x10000 >> m)) ? 30 : 29;
+    /**
+     * 获取农历年份某月的总天数
+     */
+    function monthDays(year, month) {
+        return (lunarInfo[year - 1900] & (0x10000 >> month)) ? 30 : 29;
     }
 
-    // 公历转农历
-    function solar2lunar(sy, sm, sd) {
-        if (sy < 1900 || sy > 2100) return null;
+    /**
+     * 公历转农历
+     */
+    function solarToLunar(date) {
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        
         var baseDate = new Date(1900, 0, 31);
-        var objDate = new Date(sy, sm - 1, sd);
-        var offset = Math.floor((objDate - baseDate) / 86400000);
-
-        var i, temp = 0;
-        for (i = 1900; i < 2101 && offset > 0; i++) {
-            temp = lYearDays(i);
-            offset -= temp;
+        var offset = Math.floor((date - baseDate) / 86400000);
+        
+        var lunarYear = 1900;
+        var daysOfYear = 0;
+        
+        for (var i = 1900; i < 2100 && offset > 0; i++) {
+            daysOfYear = lYearDays(i);
+            offset -= daysOfYear;
+            lunarYear++;
         }
+        
         if (offset < 0) {
-            offset += temp;
-            i--;
+            offset += daysOfYear;
+            lunarYear--;
         }
-        var year = i;
-        var leap = leapMonth(i);
+        
+        var lunarMonth = 1;
+        var lunarDay = 1;
         var isLeap = false;
-
-        for (i = 1; i < 13 && offset > 0; i++) {
+        var leap = leapMonth(lunarYear);
+        var daysOfMonth = 0;
+        
+        for (var i = 1; i < 13 && offset > 0; i++) {
             if (leap > 0 && i === (leap + 1) && !isLeap) {
-                --i;
+                i--;
                 isLeap = true;
-                temp = leapDays(year);
+                daysOfMonth = leapDays(lunarYear);
             } else {
-                temp = monthDays(year, i);
+                daysOfMonth = monthDays(lunarYear, i);
             }
-            if (isLeap && i === (leap + 1)) isLeap = false;
-            offset -= temp;
+            
+            if (isLeap && i === (leap + 1)) {
+                isLeap = false;
+            }
+            
+            offset -= daysOfMonth;
+            lunarMonth++;
         }
+        
         if (offset === 0 && leap > 0 && i === leap + 1) {
             if (isLeap) {
                 isLeap = false;
             } else {
                 isLeap = true;
-                --i;
+                i--;
             }
         }
+        
         if (offset < 0) {
-            offset += temp;
-            --i;
+            offset += daysOfMonth;
+            lunarMonth--;
         }
-        var month = i;
-        var day = offset + 1;
-
-        // 天干地支年
-        var ganIdx = (year - 4) % 10;
-        var zhiIdx = (year - 4) % 12;
-
+        
+        lunarDay = offset + 1;
+        
         return {
-            lYear: year,
-            lMonth: month,
-            lDay: day,
+            year: lunarYear,
+            month: lunarMonth,
+            day: lunarDay,
             isLeap: isLeap,
-            yearGanZhi: tianGan[ganIdx] + diZhi[zhiIdx],
-            shengXiao: shengXiao[zhiIdx],
-            monthCN: (isLeap ? '闰' : '') + lunarMonthName[month - 1] + '月',
-            dayCN: lunarDayName[day - 1],
-            fullCN: tianGan[ganIdx] + diZhi[zhiIdx] + '年 ' + (isLeap ? '闰' : '') + lunarMonthName[month - 1] + '月' + lunarDayName[day - 1]
+            yearGanZhi: getYearGanZhi(lunarYear),
+            monthGanZhi: getMonthGanZhi(lunarYear, lunarMonth),
+            dayGanZhi: getDayGanZhi(date),
+            animal: animals[(lunarYear - 4) % 12],
+            monthName: (isLeap ? '闰' : '') + lunarMonths[lunarMonth - 1] + '月',
+            dayName: lunarDays[lunarDay - 1]
         };
     }
 
-    return {
-        solar2lunar: solar2lunar,
-        tianGan: tianGan,
-        diZhi: diZhi,
-        shengXiao: shengXiao
+    /**
+     * 获取年干支
+     */
+    function getYearGanZhi(year) {
+        var ganIndex = (year - 4) % 10;
+        var zhiIndex = (year - 4) % 12;
+        return gan[ganIndex] + zhi[zhiIndex];
+    }
+
+    /**
+     * 获取月干支
+     */
+    function getMonthGanZhi(year, month) {
+        var yearGanIndex = (year - 4) % 10;
+        var monthGanIndex = (yearGanIndex * 2 + month) % 10;
+        var monthZhiIndex = (month + 1) % 12;
+        return gan[monthGanIndex] + zhi[monthZhiIndex];
+    }
+
+    /**
+     * 获取日干支
+     */
+    function getDayGanZhi(date) {
+        var baseDate = new Date(1900, 0, 31);
+        var offset = Math.floor((date - baseDate) / 86400000);
+        var ganIndex = (offset + 40) % 10;
+        var zhiIndex = (offset + 16) % 12;
+        return gan[ganIndex] + zhi[zhiIndex];
+    }
+
+    /**
+     * 获取时辰干支
+     */
+    function getHourGanZhi(dayGanZhi, hour) {
+        var dayGan = dayGanZhi.charAt(0);
+        var dayGanIndex = gan.indexOf(dayGan);
+        var hourZhiIndex = Math.floor((hour + 1) / 2) % 12;
+        var hourGanIndex = (dayGanIndex * 2 + hourZhiIndex) % 10;
+        return gan[hourGanIndex] + zhi[hourZhiIndex];
+    }
+
+    /**
+     * 获取节气
+     */
+    function getSolarTerm(year, month, day) {
+        var termInfo = [
+            6, 20, 4, 19, 6, 21, 5, 20, 6, 21, 6, 22, 7, 23, 8, 23, 8, 23, 8, 24, 8, 23, 7, 22
+        ];
+        var termIndex = (month - 1) * 2;
+        if (day === termInfo[termIndex]) {
+            return solarTerms[termIndex];
+        } else if (day === termInfo[termIndex + 1]) {
+            return solarTerms[termIndex + 1];
+        }
+        return null;
+    }
+
+    /**
+     * 获取八字
+     */
+    function getBazi(year, month, day, hour) {
+        var date = new Date(year, month - 1, day);
+        var lunar = solarToLunar(date);
+        var dayGanZhi = lunar.dayGanZhi;
+        var hourGanZhi = getHourGanZhi(dayGanZhi, hour);
+        
+        return {
+            year: lunar.yearGanZhi,
+            month: lunar.monthGanZhi,
+            day: dayGanZhi,
+            hour: hourGanZhi
+        };
+    }
+
+    /**
+     * 获取生肖
+     */
+    function getAnimal(year) {
+        return animals[(year - 4) % 12];
+    }
+
+    // 导出API
+    var Lunar = {
+        solarToLunar: solarToLunar,
+        getYearGanZhi: getYearGanZhi,
+        getMonthGanZhi: getMonthGanZhi,
+        getDayGanZhi: getDayGanZhi,
+        getHourGanZhi: getHourGanZhi,
+        getSolarTerm: getSolarTerm,
+        getBazi: getBazi,
+        getAnimal: getAnimal,
+        gan: gan,
+        zhi: zhi,
+        animals: animals
     };
-})();
+
+    // 兼容CommonJS和浏览器
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Lunar;
+    } else {
+        global.Lunar = Lunar;
+    }
+
+})(typeof window !== 'undefined' ? window : this);
